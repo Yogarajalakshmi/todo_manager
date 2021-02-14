9fddf0b91f5d60b7a6d94b4f02f29ef113d88681
Yogarajalakshmi <17euec176@skcet.ac.in>
2021-02-14 12:57:57 +0530

    Updated page using erb

--- app/models/todo.rb
+++ app/models/todo.rb
@@ -1,6 +1,43 @@
 class Todo < ActiveRecord::Base
-  def to_pleasant_string
-    is_completed = completed ? "[X]" : "[ ]"
-    "#{id}. #{due_date.to_s(:long)} #{todo_text} #{is_completed}"
+  def due_today?
+    due_date == Date.due_today
+  end
+
+  def self.overdue
+    all.where("due_date < ?", Date.today)
+  end
+
+  def self.due_today
+    all.where("due_date = ?", Date.today)
+  end
+
+  def self.due_later
+    all.where("due_date > ?", Date.today)
+  end
+
+  def self.completed
+    all.where(completed: true)
+  end
+
+  def self.show_list
+    puts "My Todo-list\n\n"
+    puts "Overdue\n"
+    puts overdue.show_list
+    puts "\n\n"
+
+    puts "Due Today\n"
+    puts due_today.show_list
+    puts "\n\n"
+
+    puts "Due Later\n"
+    puts due_later.show_list
+    puts "\n\n"
+  end
+
+  def self.mark_as_complete!(id)
+    todo = find(id)
+    todo.completed = true
+    todo.save!
+    todo
   end
 end
