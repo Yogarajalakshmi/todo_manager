dc126870aca49fedbca083de049a8fb4708d2f42
Yogarajalakshmi <17euec176@skcet.ac.in>
2021-02-23 11:45:32 +0530

    Deployed in Heroku

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,5 +1,7 @@
 #todos_controller.rb
 class TodosController < ApplicationController
+  #skip_before_filter :verify_authenticity_token
+
   def index
     #render plain: Todo.order(:due_date).map { |todo| todo.to_pleasant_string }.join("\n")
     render "index"
